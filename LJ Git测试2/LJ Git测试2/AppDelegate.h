b7877667fd78229814eb873666b83fe92801b87c
李江岩 <LJ@lijiangyandeMacBook-Pro.local>
2015-08-17 13:26:59 +0800

    LJ的Git 测试文件2

---
+++ LJ Git测试2/LJ Git测试2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJ Git测试2
+//
+//  Created by 李江岩 on 15/8/17.
+//  Copyright (c) 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJ Git测试2/LJ Git测试2Tests/LJ_Git__2Tests.m
@@ -0,0 +1,40 @@
+//
+//  LJ_Git__2Tests.m
+//  LJ Git测试2Tests
+//
+//  Created by 李江岩 on 15/8/17.
+//  Copyright (c) 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LJ_Git__2Tests : XCTestCase
+
+@end
+
+@implementation LJ_Git__2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
