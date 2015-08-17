57506da92c5027a3b0b9c0d74642ba44f3988ee2
李江岩 <LJ@lijiangyandeMacBook-Pro.local>
2015-08-17 13:24:52 +0800

    新建的Git测试文件

---
+++ LJ Git测试/LJ Git测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LJ Git测试
+//
+//  Created by 李江岩 on 15/8/17.
+//  Copyright (c) 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
